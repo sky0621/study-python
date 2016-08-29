4d8f19c5e0447680a507e070dea57ed71d70d4ae
S.S <ss@ss.com>
2016-08-30 00:48:11 +0900

    retry

---
+++ tutorial/retry/a.py
@@ -0,0 +1,24 @@
+print('abcde')
+
+pgs = ('Java', 'Python', 'Ruby', 'Go')
+print(pgs[:2])
+print(pgs[1:3])
+print(pgs[1:])
+print(len(pgs))
+
+a = 'Py' 'tho' 'n'
+print(a)
+
+b = 'GOGO'
+b = 'JEJE'
+print(b)
+
+
+# 0 1 1 2 3 5 8 13 21 34
+bef, now = 0, 1
+while now < 100:
+    print(now)
+    bef, now = now, bef + now
+
+words = ['C', 'C++', 'C#', 'PHP', 'Perl', 'Python', 'Elixir', 'Go', 'Java', 'Scala', 'Gloovy', 'Ruby', 'JavaScript', 'Lisp']
+print(len(words))
